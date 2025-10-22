cd828d7af0b4c2e85a93aa57a47a44e701d8d22a
BuildTools <unconfigured@null.spigotmc.org>
2025-10-21 23:31:41 -0700

    Pin Example

--- frontend/src/map/Map.jsx
+++ frontend/src/map/Map.jsx
@@ -1,5 +1,25 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+// --- Example pin data ---
+function makeIcon(className) {
+  return new L.Icon({
+    iconUrl,
+    iconRetinaUrl,
+    shadowUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    className,
+  });
+}
+
+const position1 = [36.98946, -122.06124];
+const position2 = [36.99456, -122.05432];
 
 const bounds = [
   [36.972521, -122.071543], // southwest corner
@@ -22,6 +42,29 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {/* CSS filters to recolor the default marker icon */}
+      <style>{`
+        .leaflet-marker-icon.marker-blue { filter: hue-rotate(0deg); }
+        .leaflet-marker-icon.marker-green { filter: hue-rotate(250deg); }
+        .leaflet-marker-icon.marker-red  { filter: hue-rotate(130deg); }
+      `}</style>
+
+      {/* Example markers in different colors */}
+      <Marker position={position1} icon={makeIcon("marker-blue")}>
+        <Popup>
+          <b>Custom colored marker</b><br />
+          Category: Example<br />
+          Detail: Example
+        </Popup>
+      </Marker>
+
+      <Marker position={position2} icon={makeIcon("marker-red")}>
+        <Popup>
+          <b>Alert</b><br />
+          Category: Safety<br />
+          Detail: Example red pin
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
