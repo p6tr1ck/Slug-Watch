852a190513c69835deb60ac3d6487202a50bce40
BuildTools <unconfigured@null.spigotmc.org>
2025-10-24 15:43:07 -0700

    added ViewportController and loadPinsFromBackend()

--- frontend/src/map/Map.jsx
+++ frontend/src/map/Map.jsx
@@ -1,11 +1,11 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState, useMemo } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
 
-// --- Example pin data ---
 function makeIcon(className) {
   return new L.Icon({
     iconUrl,
@@ -18,6 +18,49 @@ function makeIcon(className) {
   });
 }
 
+function getIconForCategory(category) {
+  switch (category) {
+    case "vehicle accident":
+      return makeIcon("marker-red");
+    case "suspicious activity":
+      return makeIcon("marker-blue");
+    case "animal":
+      return makeIcon("marker-green");
+    default:
+      return makeIcon();
+  }
+}
+
+function getBBoxFromMap(map) {
+  const b = map.getBounds();
+  return [b.getWest(), b.getSouth(), b.getEast(), b.getNorth()];
+}
+
+function ViewportController({ onViewportChange }) {
+  const map = useMap();
+
+  useMapEvents({
+    moveend() {
+      const bbox = getBBoxFromMap(map);
+      onViewportChange(bbox);
+    },
+    zoomend() {
+      const bbox = getBBoxFromMap(map);
+      onViewportChange(bbox);
+    },
+  });
+
+  const [didInitialLoad] = useState(() => {
+    const bbox = getBBoxFromMap(map);
+    onViewportChange(bbox);
+    return true;
+  });
+
+  void didInitialLoad;
+
+  return null;
+}
+
 const position1 = [36.98946, -122.06124];
 const position2 = [36.99456, -122.05432];
 
@@ -27,6 +70,52 @@ const bounds = [
 ];
 
 export default function Map() {
+  const [pins, setPins] = useState([]);
+
+  function loadPinsFromBackend(bbox) {
+    const mock = [
+      {
+        id: "1",
+        lat: 36.98946,
+        lng: -122.06124,
+        title: "Suspicious Individual at Science Hill",
+        category: "suspicious activity",
+        description:
+          "Individual reported acting suspiciously reported near Science Hill around 10pm.",
+      },
+      {
+        id: "2",
+        lat: 36.99182,
+        lng: -122.05897,
+        title: "Stray Animal at Porter College",
+        category: "animal",
+        description:
+          "Report of a stray dog running near Porter College dorm area.",
+      },
+      {
+        id: "3",
+        lat: 36.99501,
+        lng: -122.05642,
+        title: "Vehicle Accident at Main Entrance",
+        category: "vehicle accident",
+        description:
+          "Minor collision near the main campus entrance.",
+      }
+    ];
+    setPins(mock);
+  }
+
+  const ColorFilters = useMemo(
+    () => (
+      <style>{`
+        .leaflet-marker-icon.marker-blue { filter: hue-rotate(0deg); }
+        .leaflet-marker-icon.marker-green { filter: hue-rotate(250deg); }
+        .leaflet-marker-icon.marker-red  { filter: hue-rotate(130deg); }
+      `}</style>
+    ),
+    []
+  );
+
   return (
     <MapContainer
       center={[36.992255, -122.058763]}
@@ -38,33 +127,35 @@ export default function Map() {
       zoomControl={false} // remove zoom in/out buttons
       zoomSnap={0.1} // allow decimal zooming by 0.1, 0.2, 0.3, etc.
     >
+
+      {ColorFilters}
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* CSS filters to recolor the default marker icon */}
-      <style>{`
-        .leaflet-marker-icon.marker-blue { filter: hue-rotate(0deg); }
-        .leaflet-marker-icon.marker-green { filter: hue-rotate(250deg); }
-        .leaflet-marker-icon.marker-red  { filter: hue-rotate(130deg); }
-      `}</style>
+      
+      <ViewportController onViewportChange={loadPinsFromBackend} />
 
-      {/* Example markers in different colors */}
-      <Marker position={position1} icon={makeIcon("marker-blue")}>
-        <Popup>
-          <b>Custom colored marker</b><br />
-          Category: Example<br />
-          Detail: Example
-        </Popup>
-      </Marker>
-
-      <Marker position={position2} icon={makeIcon("marker-red")}>
-        <Popup>
-          <b>Alert</b><br />
-          Category: Safety<br />
-          Detail: Example red pin
-        </Popup>
-      </Marker>
+      {pins.map((pin) => (
+        <Marker
+          key={pin.id}
+          position={[pin.lat, pin.lng]}
+          icon={getIconForCategory(pin.category)}
+        >
+          <Popup>
+            <b>{pin.title}</b>
+            <br />
+            {pin.category ? (
+              <span>
+                <i>Category:</i> {pin.category}
+                <br />
+              </span>
+            ) : null}
+            {pin.description || "No details"}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
