6c17f9097ee5692a215215345ce916d4b98e6f11
Patrick <pfontani@ucsc.edu>
2025-10-31 00:35:40 -0700

    example of pulling pin from map

--- frontend/src/map/Map.jsx
+++ frontend/src/map/Map.jsx
@@ -4,6 +4,9 @@ import L from "leaflet";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+import { supabase } from "../../supabaseClient";
+import { useState } from "react";
+import { useEffect } from "react";
 
 // --- Example pin data ---
 function makeIcon(className) {
@@ -27,6 +30,54 @@ const bounds = [
 ];
 
 export default function Map() {
+  const [pins, setPins] = useState([]);
+
+  useEffect(() => {
+    const getPins = async () => {
+      const { data } = await supabase.from("example_pins").select();
+      data.forEach((e) => {
+        setPins([
+          ...pins,
+          {
+            id: e.id,
+            user_id: e.id,
+            title: e.title,
+            category: e.category,
+            lat: e.lat,
+            long: e.long,
+            created_at: e.created_at,
+            description: e.description,
+          },
+        ]);
+      });
+    };
+    getPins();
+  }, []);
+
+  useEffect(() => {
+    console.log(pins);
+  }, [pins]);
+
+  // useEffect(() => {
+  //   const channel = supabase.channel("example_pins_channel");
+  //   channel
+  //     .on(
+  //       "postgres_changes",
+  //       {
+  //         event: "*",
+  //         schema: "public",
+  //         table: "example_pins",
+  //       },
+  //       (payload) => {
+  //         const newPin = payload.new;
+  //         console.log(newPin);
+  //       }
+  //     )
+  //     .subscribe((status) => {
+  //       console.log("Subscription: ", status);
+  //     });
+  // }, []);
+
   return (
     <MapContainer
       center={[36.992255, -122.058763]}
@@ -48,20 +99,36 @@ export default function Map() {
         .leaflet-marker-icon.marker-green { filter: hue-rotate(250deg); }
         .leaflet-marker-icon.marker-red  { filter: hue-rotate(130deg); }
       `}</style>
-
+      {pins.map((pin) => {
+        return (
+          <Marker position={[pin.lat, pin.long]} icon={makeIcon("marker-blue")}>
+            <Popup>
+              <b>{pin.title}</b>
+              <br />
+              Category: <b>{pin.category}</b>
+              <br />
+              Description: {pin.description}
+            </Popup>
+          </Marker>
+        );
+      })}
       {/* Example markers in different colors */}
       <Marker position={position1} icon={makeIcon("marker-blue")}>
         <Popup>
-          <b>Custom colored marker</b><br />
-          Category: Example<br />
+          <b>Custom colored marker</b>
+          <br />
+          Category: Example
+          <br />
           Detail: Example
         </Popup>
       </Marker>
 
       <Marker position={position2} icon={makeIcon("marker-red")}>
         <Popup>
-          <b>Alert</b><br />
-          Category: Safety<br />
+          <b>Alert</b>
+          <br />
+          Category: Safety
+          <br />
           Detail: Example red pin
         </Popup>
       </Marker>
