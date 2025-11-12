c1cae227b4bb3b79edc5c209fd9e5fa03798eb60
Patrick <pfontani@ucsc.edu>
2025-11-11 22:18:41 -0800

    created suspicious person icon

--- frontend/src/map/makeIcon.js
+++ frontend/src/map/makeIcon.js
@@ -3,11 +3,22 @@ import iconUrl from "leaflet/dist/images/marker-icon.png";
 import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
 import PoliceCar from "../assets/police-car-emoji.png";
+import SuspiciousPerson from "../assets/suspicious-person-icon.png";
 
 const tapsIcon = (category) => {
   return new L.Icon({
     iconUrl: PoliceCar,
     iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [1, -34],
+    category,
+  });
+};
+
+const suspiciousPerson = (category) => {
+  return new L.Icon({
+    iconUrl: SuspiciousPerson,
+    iconSize: [48, 48],
     iconAnchor: [16, 32],
     popupAnchor: [1, -34],
     category,
@@ -29,6 +40,8 @@ const blueMarker = (category) => {
 export default function makeIcon(category) {
   if (category == "TAPS") {
     return tapsIcon(category);
+  } else if (category == "Suspicious Activity") {
+    return suspiciousPerson(category);
   } else {
     return blueMarker(category);
   }
