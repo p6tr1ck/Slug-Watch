ebee74a7be43fc8c0f9da67cb6cf1d7c00e49e0b
Patrick <pfontani@ucsc.edu>
2025-11-16 22:54:56 -0800

    Real time changes appear on app

--- frontend/src/map/UserPins.jsx
+++ frontend/src/map/UserPins.jsx
@@ -3,11 +3,65 @@ import { supabase } from "../../supabaseClient";
 import { AuthContext } from "../App";
 import MakeMarker from "./MakeMarker";
 
+function mapPin(data) {
+  return {
+    id: data.id,
+    user_id: data.user_id,
+    title: data.title,
+    category: data.category,
+    lat: data.lat,
+    long: data.long,
+    created_at: `${new Date(data.created_at).toLocaleDateString()} ${new Date(
+      data.created_at
+    ).toLocaleTimeString()}`, // Format as MM/DD/YY Time
+    description: data.description,
+  };
+}
+
 export default function UserPins() {
   const { session, viewMyPins, viewPolicePins } = useContext(AuthContext);
   const [pins, setPins] = useState([]);
 
-  // Pull user made pins from the database.
+  // subscribe to real time changes
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime:example_pins")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "example_pins",
+        },
+        (payload) => {
+          if (payload.eventType === "INSERT") {
+            setPins((pins) => [...pins, mapPin(payload.new)]);
+          }
+
+          if (payload.eventType === "UPDATE") {
+            // replace the pin
+            setPins((pins) =>
+              pins.map((p) =>
+                p.id === payload.new.id ? mapPin(payload.new) : p
+              )
+            );
+          }
+
+          if (payload.eventType === "DELETE") {
+            // remove it
+            setPins((pins) => pins.filter((p) => p.id !== payload.old.id));
+          }
+        }
+      )
+      .subscribe();
+
+    // cleanup on unmount
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+
+  // Initial pull of user made pins from the database.
   useEffect(() => {
     async function getPins() {
       // Select all the rows from the example pins database table.
@@ -18,18 +72,9 @@ export default function UserPins() {
       }
 
       // Create an array of all the pins fetched from the database table.
-      const mapped = data.map((e) => ({
-        id: e.id,
-        user_id: e.user_id,
-        title: e.title,
-        category: e.category,
-        lat: e.lat,
-        long: e.long,
-        created_at: `${new Date(e.created_at).toLocaleDateString()} ${new Date(
-          e.created_at
-        ).toLocaleTimeString()}`, // Format as MM/DD/YY Time
-        description: e.description,
-      }));
+      const mapped = data.map((e) => {
+        return mapPin(e);
+      });
 
       // Now set the pins, so the pins array has the pin data.
       setPins(mapped);
