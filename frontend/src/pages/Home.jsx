ad6ece49add2aa222c55462d3bb233228713e0ff
Inha Bae <baeinha030603@gmail.com>
2025-12-03 18:36:00 -0800

    remove bottom white bar on desktop mode

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Map from "../map/Map";
+import useWindowDimensions from "../WindowDimensions";
 
 export default function Home() {
+  const { width } = useWindowDimensions();
+  const isMobile = width <= 600;
+
   return (
-    <div className="flex-1 min-h-0 flex flex-col pb-20">
+    <div className={`flex-1 min-h-0 flex flex-col ${isMobile ? 'pb-20' : ''}`}>
       <div className="flex-1 min-h-0">
         <Map className="h-full w-full" />
       </div>
